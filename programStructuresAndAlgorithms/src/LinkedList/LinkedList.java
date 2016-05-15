76d7921472e6b9f5f5e7aa63a3a57f55ec91615a
Devashri Deshmuk <Devashri@10.110.143.182>
2016-05-15 13:19:43 -0400

    modified linkedList class

--- programStructuresAndAlgorithms/src/LinkedList/LinkedList.java
+++ programStructuresAndAlgorithms/src/LinkedList/LinkedList.java
@@ -4,10 +4,53 @@ public class LinkedList {
 		
 	Node first;
 	
+	//constructor
 	public LinkedList() {
 		first=null;
 	}
 	
+	//check if linked list is empty
+	public boolean isEmpty(){
+		return (first==null);
+	}
+	
+	//insert node at first 
+	public void insertNodeAtFirst(int data){
+		Node nodeToBeInserted = new Node(data);
+		if(isEmpty()){
+			this.first=nodeToBeInserted;
+			nodeToBeInserted.next=null;
+		}else{
+			nodeToBeInserted.next=first;
+			first = nodeToBeInserted;
+		}
+	}
+	
+	//print all linked list elements
+	public void printAllElements(){
+		
+		Node current = first;
+		StringBuffer buffer = new StringBuffer();
+		if(isEmpty()){
+			System.out.println("List is empty");
+			return;
+		}else{
+			System.out.println("\n");
+			while (current!=null) {
+				buffer.append(current.data);
+				buffer.append("==>");
+				current=current.next;
+			}
+		}
+		
+		int length = buffer.length();
+		buffer.deleteCharAt(length-1);
+		buffer.deleteCharAt(length-2);
+		buffer.deleteCharAt(length-3);
+		
+		
+		System.out.println(buffer.toString());
+	}
 	
 		
 }
--- programStructuresAndAlgorithms/src/test/RunnerLinkedList.java
+++ programStructuresAndAlgorithms/src/test/RunnerLinkedList.java
@@ -1,10 +1,19 @@
 package test;
 
-
+import LinkedList.LinkedList;
 
 public class RunnerLinkedList {
 	
 	public static void main(String[] args) {
+		
+		LinkedList linkedList = new LinkedList();
+		linkedList.printAllElements();
+		
+		linkedList.insertNodeAtFirst(6);
+		linkedList.insertNodeAtFirst(4);
+		linkedList.insertNodeAtFirst(2);
+		linkedList.insertNodeAtFirst(1);
+		linkedList.printAllElements();
 		
 	}
 	
