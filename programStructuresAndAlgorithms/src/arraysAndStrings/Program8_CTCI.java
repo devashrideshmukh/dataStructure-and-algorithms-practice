8bd7adf87aaf4d7348dc06cb5f834cc2d741988c
Devashri Deshmuk <Devashri@10.110.143.182>
2016-08-07 10:56:49 -0400

    set zeros

--- programStructuresAndAlgorithms/src/arraysAndStrings/Program8_CTCI.java
+++ programStructuresAndAlgorithms/src/arraysAndStrings/Program8_CTCI.java
@@ -1,5 +1,7 @@
 package arraysAndStrings;
 
+import utilityPackage.Utility;
+
 /**
  * Zero Matrix Write an algorithm such that if an element in an MxN matrix is 0,
  * its entire row and column are set to 0
@@ -47,7 +49,7 @@ public class Program8_CTCI {
 	 * Method 2 (Optimized)
 	 * @param matrix
 	 */
-	public void setZeroes(int[][] matrix) {
+	public static void setZeroesMethod2(int[][] matrix) {
 
 		boolean rowHasZero = false;
 		boolean colHasZero = false;
@@ -81,7 +83,7 @@ public class Program8_CTCI {
 			}
 		}
 
-		for (int j = 0; j < matrix.length; j++) {
+		for (int j = 1; j < matrix.length; j++) {
 			if (matrix[j][0] == 0) {
 				nullifyRow(matrix, j);
 			}
@@ -97,16 +99,28 @@ public class Program8_CTCI {
 
 	}
 
-	public void nullifyRow(int[][] matrix, int row) {
+	public static void nullifyRow(int[][] matrix, int row) {
 		for (int i = 0; i < matrix[0].length; i++) {
 			matrix[row][i] = 0;
 		}
 	}
 
-	public void nullifyColumn(int[][] matrix, int col) {
+	public static void nullifyColumn(int[][] matrix, int col) {
 		for (int i = 0; i < matrix.length; i++) {
 			matrix[i][col] = 0;
 		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("==========Zero matrix problem=================================");
+		int[][] matrix1 = { { 1, 2, 3, 4, 0, 6 }, { 1, 2, 3, 4, 0, 6 }, { 7, 8, 9, 10, 0, 12 },
+				{ 13, 14, 15, 16, 17, 18 }, { 19, 20, 21, 22, 23, 24 }, { 25, 26, 27, 28, 29, 30 },
+				{ 31, 32, 33, 34, 35, 36 } };
+		Utility.printMatrix(matrix1);
+		System.out.println("==================================================");
+		Program8_CTCI.setZeroesMethod2(matrix1);
+		Utility.printMatrix(matrix1);
+		System.out.println("==================================================");
 	}
 
 }
